4c5da5d8a145be7467b2b65df91b6cb613448b5f
Felipe Müller <felipesilvamuller@Hotmail.com>
2023-10-19 10:04:50 -0300

    Add files via upload

---
+++ KotlinExercicio.kt
@@ -0,0 +1,14 @@
+fun main() {
+	
+	
+	//fun simboliza função
+//Variaveis em Kotlin.
+
+
+	// var  userName : //tipo da variavel
+
+	var userName: String = "Felipe";
+
+	print("My name is $userName!");
+
+}
